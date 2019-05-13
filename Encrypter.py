31639c57583f837de4008f12def18c4930501121
alik604 <alik9807274@gmail.com>
2019-05-12 21:54:07 -0700

    add keylogger

---
+++ Encrypter.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python3
+# used: https://stackoverflow.com/a/20868265
+# additionally: https://pypi.org/project/pyAesCrypt/
+#
+from Crypto import Random
+from Crypto.Cipher import AES
+
+def pad(s):
+    return s + b"\0" * (AES.block_size - len(s) % AES.block_size)
+
+
+def encrypt(message, key, key_size=256):
+    message = pad(message)
+    iv = Random.new().read(AES.block_size)
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    return iv + cipher.encrypt(message)
+
+
+def decrypt(ciphertext, key):
+    iv = ciphertext[:AES.block_size]
+    cipher = AES.new(key, AES.MODE_CBC, iv)
+    plaintext = cipher.decrypt(ciphertext[AES.block_size:])
+    return plaintext.rstrip(b"\0")
+
+
+def encrypt_file(file_name, key):
+    with open(file_name, 'rb') as fo:
+        plaintext = fo.read()
+    enc = encrypt(plaintext, key)
+    with open(file_name + ".enc", 'wb') as fo:
+        fo.write(enc)
+
+
+def decrypt_file(file_name, key):
+    with open(file_name, 'rb') as fo:
+        ciphertext = fo.read()
+    dec = decrypt(ciphertext, key)
+    with open(file_name[:-4], 'wb') as fo:
+        fo.write(dec)
+
+
+key = b'\xbf\xc0\x85)\x10nc\x94\x02)j\xdf\xcb\xc4\x94\x9d(\x9e[EX\xc8\xd5\xbfI{\xa2$\x05(\xd5\x18'
+
+encrypt_file('to_enc.txt', key)
+#decrypt_file('to_enc.txt.enc', key)
