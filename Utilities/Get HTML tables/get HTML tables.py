c30a63e030afee899102ab53a681bc2f4d6663fb
Alik604 <alik9807274@gmail.com>
2020-08-09 12:58:31 -0700

    Update get HTML tables.py

--- Utilities/Get HTML tables/get HTML tables.py
+++ Utilities/Get HTML tables/get HTML tables.py
@@ -9,20 +9,26 @@ Total time: 1:40 + 0:50 = 2:30
 '''
 
 import urllib.request
+
 import pandas as pd
 
-# URL = "http://www.python.org"
+URL = "http://www.python.org"
+# URL = "https://www.w3schools.com/html/html_tables.asp"
 # URL = "https://en.wikipedia.org/wiki/Maginot_Line"
-# URL = "https://ca.finance.yahoo.com/quote/AAPL/financials?p=AAPL"  # AAPL Stock
-URL = "https://www.w3schools.com/html/html_tables.asp"
+
+## hard
+# URL = "https://ca.finance.yahoo.com/quote/AAPL/cash-flow?p=AAPL"  # AAPL Stock
+# URL = 'http://finance.yahoo.com/q?s=aapl&ql=1'
+
+# htmlSplit = urllib.request.urlopen('http://finance.yahoo.com/q?s=aapl&ql=1').read().decode("utf8")
+
+
 fp = urllib.request.urlopen(URL)
 mybytes = fp.read()
-
 mystr = mybytes.decode("utf8")
-
 fp.close()
 
-htmlSplit = mystr.split('\n')
+htmlSplit = mystr.split('\n')  # .replace('>','>\n')
 # for i in htmlSplit:
 #     print(i)
 print(type(htmlSplit))  # list
@@ -31,12 +37,12 @@ print(type(htmlSplit))  # list
 def delete_tag_with_prarams(data, first_bit_of_first_tag='<a', closing_tag='</a>'):
     start_index = data.find(first_bit_of_first_tag)
     if start_index != -1:
-        print('delete_tag_with_prarams PRE: ', data)
+        # print('delete_tag_with_prarams PRE: ', data)
         end_index = data.find('>')
         # print(start_index, " | ", end_index)
         to_remove = data[start_index:end_index + 1]
         data = data.replace(to_remove, '').replace(closing_tag, '')
-        print('delete_tag_with_prarams POST: ', data)
+        # print('delete_tag_with_prarams POST: ', data)
     return data
 
 
@@ -44,23 +50,37 @@ def clean(data):
     if len(data) == 0:
         return ''
 
-    # print('will clean: ', data)
-
     if data[0] == '>':
         data = data[1:]
+    data = data.replace('><', '> <')
+    data = data.replace('">', ' >')
 
-    strings_to_remove = ['<p>', '</p>', '<em>', '</em>', '<a>', '&lt;', '&gt;', '&le;', '&ge;']
+    strings_to_remove = ['<p>', '</p>', '<em>', '</em>', '<a>', '<tr>', '</tr>', '&lt;', '&gt;', '&le;', '&ge;']
     for i in strings_to_remove:
         data = data.replace(i, '')
 
     print('cleaning: ', data)
-    data_copy = data
-    data = delete_tag_with_prarams(data, '<a', '</a>')
-    while data != data_copy:
+    while True:
         data_copy = data
         data = delete_tag_with_prarams(data, '<a', '</a>')
+        data = delete_tag_with_prarams(data, '<th', '</th>')
+        data = delete_tag_with_prarams(data, '<td', '</td>')
+        data = delete_tag_with_prarams(data, '<tbody', '</tbody>')
+        data = delete_tag_with_prarams(data, '<span', '</span>')
+        data = delete_tag_with_prarams(data, '<i', '</i>')
         # data = delete_tag_with_prarams(data, '<table', '</table>')
+
+        if '<th' in data:
+            print('True')
+            start_index = data.find('>')
+            end_index = data.find('</')
+            data = data[start_index:end_index]
+            data = clean(data)
+
         print('it ran...')
+
+        if data == data_copy:
+            break
     print('returning: ', data)
     return data
 
@@ -68,33 +88,33 @@ def clean(data):
 def process(lines):
     list_of_tables = []
     nested_array = []
-    data = ''
-    print('--------------------------------------------------------------------------------------------------------------------')
+    print('------------------------------------------------------------------------------------------------------')
     for line in lines:
-
         if '</tr>' in line:
             list_of_tables.append(nested_array)
             nested_array = []
 
         nest = ''
-        data = ''
+
         if '<th' in line:
             start_index = line.find('>')
             end_index = line.find('</')
-            data += line[start_index:end_index]
-
+            data = line[start_index:end_index]
             nest += clean(data)
-            data = ''
-        if '<td' in line:
-            end_index = line.rfind('</')
-            start_index = line.find('>')  # the last '>' is always the last index, so have a upper bound
-            print(start_index, " | ", end_index)
-            data += line[start_index + 1:end_index]
-            nest += clean(data)
-
-        # next to create: list of tables
-        if len(nest) > 0:
             nested_array.append(nest)
+
+        if '<td' in line:
+            start_index = line.find('>')
+            end_index = line.rfind('</')
+
+            print(start_index, " | ", end_index)
+            data = line[start_index + 1:end_index]
+            nest += clean(data)
+            nested_array.append(nest)
+
+        # # next to create: list of tables
+        # if len(nest) > 0:
+        #     nested_array.append(nest)
 
     return list_of_tables
 
@@ -110,7 +130,7 @@ for line in htmlSplit:
         lines = []  # should* be clear in end instead, but will effect final print
     if flip:
         # show data
-        print(line)
+        # print(line)
         lines.append(line)
     if '</table>' in line:
         flip = False
@@ -122,4 +142,4 @@ for line in htmlSplit:
 
 print('=========================================================')
 for array in list_of_nested_arrays:
-   print(pd.DataFrame(array).head())
+    print(pd.DataFrame(array).head())
